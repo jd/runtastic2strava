368af44f2bd84a8608accdb8e68e94faa069bd45
Julien Danjou <julien@danjou.info>
2015-03-26 19:20:58 +0100

    Fix script wrt recent changes of Runtastic
    Now relies on requests rather than mechanize

--- runtastic2strava.py
+++ runtastic2strava.py
@@ -7,7 +7,7 @@ import re
 import smtplib
 
 from lxml import html
-import mechanize
+import requests
 import six
 from six.moves import email_mime_base
 from six.moves import email_mime_multipart
@@ -18,17 +18,15 @@ with open("settings.json", "r") as f:
 
 STRAVA_UPLOAD = "upload@strava.com"
 
-br = mechanize.Browser()
-br.open("http://runtastic.com")
-br.select_form(nr=0)
-br['user[email]'] = settings['runtastic_email']
-br['user[password]'] = settings['runtastic_pass']
-br.submit()
 
-resp = br.open(
-    "https://www.runtastic.com/en/users/%s/sport-sessions"
-    % settings['runtastic_username'])
-tree = html.fromstring(resp.read())
+login = requests.post("https://www.runtastic.com/en/d/users/sign_in",
+                      data={"user[email]": settings['runtastic_email'],
+                            "user[password]": settings['runtastic_pass']})
+resp = requests.get("https://www.runtastic.com/en/users/%s/sport-sessions"
+                    % settings['runtastic_username'],
+                    cookies=login.cookies)
+
+tree = html.fromstring(resp.text)
 js = tree.xpath('//script')[17].text
 activities = json.loads(re.search(r"index_data = ([^;]+);", js).group(1))
 
@@ -41,21 +39,21 @@ s = smtplib.SMTP('localhost')
 # Only send the last N days of activities
 for activity in filter(lambda a: a[1] >= last_sync_day, activities):
     activity_id = activity[0]
-    resp = br.open(
+    resp = requests.get(
         "https://www.runtastic.com/en/users/%s/sport-sessions/%s.tcx"
-        % (settings['runtastic_username'], activity_id))
+        % (settings['runtastic_username'], activity_id),
+        cookies=login.cookies)
     msg = email_mime_multipart.MIMEMultipart()
     msg['From'] = settings['strava_user']
     msg['To'] = STRAVA_UPLOAD
     msg['Subject'] = six.text_type(activity_id)
     msg['Date'] = utils.formatdate()
     part = email_mime_base.MIMEBase("application", "octet-stream")
-    payload = resp.read()
     filename = "%s.tcx" % activity_id
     # Save the file locally, just in case.
     with file("archives/" + filename, "w") as f:
-        f.write(payload)
-    part.set_payload(payload)
+        f.write(resp.text)
+    part.set_payload(resp.text)
     encoders.encode_base64(part)
     part.add_header('Content-Disposition',
                     'attachment; filename="%s"'
