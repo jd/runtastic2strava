9c7b754e9eb7121530e067814ca947bca615e2ec
Julien Danjou <julien@danjou.info>
2015-05-19 15:52:25 -0700

    Use API rather than mail upload
    Signed-off-by: Julien Danjou <julien@danjou.info>

--- runtastic2strava.py
+++ runtastic2strava.py
@@ -1,16 +1,12 @@
 #!/usr/bin/env python
 import datetime
-from email import encoders
-from email import utils
 import json
 import re
-import smtplib
 
 from lxml import html
 import requests
 import six
-from six.moves import email_mime_base
-from six.moves import email_mime_multipart
+import stravalib
 
 
 with open("settings.json", "r") as f:
@@ -34,7 +30,8 @@ last_sync_day = (datetime.datetime.utcnow()
                  - datetime.timedelta(
                      days=settings.get('days_window', settings['sync_days']))).strftime("%Y-%m-%d")
 
-s = smtplib.SMTP(settings['smtp_server'])
+client = stravalib.Client(
+    access_token=settings.get("strava_access_token"))
 
 # Only send the last N days of activities
 for activity in filter(lambda a: a[1] >= last_sync_day, activities):
@@ -43,24 +40,14 @@ for activity in filter(lambda a: a[1] >= last_sync_day, activities):
         "https://www.runtastic.com/en/users/%s/sport-sessions/%s.tcx"
         % (settings['runtastic_username'], activity_id),
         cookies=login.cookies)
-    msg = email_mime_multipart.MIMEMultipart()
-    msg['From'] = settings['strava_user']
-    msg['To'] = STRAVA_UPLOAD
-    msg['Subject'] = six.text_type(activity_id)
-    msg['Date'] = utils.formatdate()
-    part = email_mime_base.MIMEBase("application", "octet-stream")
     filename = "%s.tcx" % activity_id
     # Save the file locally, just in case.
-    with file("archives/" + filename, "w") as f:
+    with file("archives/" + filename, "w+") as f:
         f.write(resp.text.encode("UTF-8"))
-
-    part.set_payload(resp.text)
-    encoders.encode_base64(part)
-    part.add_header('Content-Disposition',
-                    'attachment; filename="%s"'
-                    % filename)
-    msg.attach(part)
-    s.sendmail(settings['strava_user'], [STRAVA_UPLOAD], msg.as_string())
+        f.seek(0)
+        try:
+            client.upload_activity(f, data_type="tcx")
+        except stravalib.exc.ActivityUploadFailed as e:
+            if 'duplicate' not in six.text_type(e):
+                raise
     print("Sent activity %s from %s" % (activity_id, activity[1]))
-
-s.quit()
