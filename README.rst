73b8e040e48e1f9d009e04aa9711260b370b744a
Julien Danjou <julien@danjou.info>
2018-04-02 14:41:54 +0200

    Use env var

--- README.rst
+++ README.rst
@@ -3,9 +3,18 @@ runtastic2strava
 
 This a Python script used to send sport sessions from Runtastic_ to Strava_.
 
-To run it, copy settings-sample.json to settings.json, edit it with the correct
-values and run the script. By default it only send activities for the last four
-days, but you can extend that range if you wish to synchronize older sessions.
+To run it, export the following variables in your environment and run the
+script::
+
+  export RUNTASTIC_EMAIL="foo@example.com"
+  export RUNTASTIC_PASS="s3cur3"
+  export RUNTASTIC_USERNAME="foo"
+  export STRAVA_ACCESS_TOKEN="22kdwd021k210ncbqdwq"
+
+You can use the `get-token.py` script to get a Strava access token.
+
+By default it only send activities for the last four days, but you can
+extend that range if you wish to synchronize older sessions.
 
 .. _runtastic: http://runtastic.com
 .. _strava: http://strava.com
--- runtastic2strava.py
+++ runtastic2strava.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python
 import datetime
 import json
+import os
 import re
 import sys
 
@@ -9,21 +10,23 @@ import six
 import stravalib
 
 
-with open("settings.json", "r") as f:
-    settings = json.load(f)
-
+EMAIL = os.getenv("RUNTASTIC_EMAIL")
+PASSWORD = os.getenv("RUNTASTIC_PASS")
+USERNAME = os.getenv("RUNTASTIC_USERNAME")
+DAYS_WINDOW = os.getenv("RUNTASTIC_DAYS_WINDOW", 3)
+STRAVA_ACCESS_TOKEN = os.getenv("STRAVA_ACCESS_TOKEN")
 STRAVA_UPLOAD = "upload@strava.com"
 
 
 login = requests.post("https://www.runtastic.com/en/d/users/sign_in",
-                      data={"user[email]": settings['runtastic_email'],
-                            "user[password]": settings['runtastic_pass']})
+                      data={"user[email]": EMAIL,
+                            "user[password]": PASSWORD})
 
 if login.status_code // 100 != 2:
     print("Error logging in Runtastic, aborting")
 
 resp = requests.get("https://www.runtastic.com/en/users/%s/sport-sessions"
-                    % settings['runtastic_username'],
+                    % USERNAME,
                     cookies=login.cookies)
 
 if resp.status_code // 100 != 2:
@@ -38,18 +41,16 @@ if not match_data:
 activities = json.loads(match_data.group(1))
 
 last_sync_day = (datetime.datetime.utcnow()
-                 - datetime.timedelta(
-                     days=settings.get('days_window', 3))).strftime("%Y-%m-%d")
+                 - datetime.timedelta(days=DAYS_WINDOW)).strftime("%Y-%m-%d")
 
-client = stravalib.Client(
-    access_token=settings.get("strava_access_token"))
+client = stravalib.Client(access_token=STRAVA_ACCESS_TOKEN)
 
 # Only send the last N days of activities
 for activity in filter(lambda a: a[1] >= last_sync_day, activities):
     activity_id = activity[0]
     resp = requests.get(
         "https://www.runtastic.com/en/users/%s/sport-sessions/%s.tcx"
-        % (settings['runtastic_username'], activity_id),
+        % (USERNAME, activity_id),
         cookies=login.cookies)
     filename = "%s.tcx" % activity_id
     # Save the file locally, just in case.
